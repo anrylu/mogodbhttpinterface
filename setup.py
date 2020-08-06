23bf6eda558a92380e378450da18f3d6a233c266
Anry Lu <anrylu@qnap.com>
2020-08-06 10:35:17 +0800

    modify for python2 compatibility

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import re
 
 from setuptools import setup
 
-with open("mongodbhttpinterface/__init__.py", encoding="utf8") as f:
+with open("mongodbhttpinterface/__init__.py") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name="MongoDB HTTP Interface",
+    name="mongodbhttpinterface",
     version=version,
+    author="Anry Lu",
+    author_email="anrylu@qnap.com",
     install_requires=[
         "pymongo",
         "flask",
